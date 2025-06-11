b4cdc07f0f17e46a8f5970a10e74d8ef8fcda6df
Luka929-del <luka.natsvlishvili.1@btu.edu.ge>
2025-06-11 21:22:19 +0400

    wrote some code in JS file
    I wrote some code in JS file in order to solve the exercise that was given.

---
+++ script.js
@@ -0,0 +1,13 @@
+function getDivisors(number) {
+    const divisors = [];
+
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            divisors.push(i);
+        }
+    }
+
+    return divisors;
+}
+
+console.log(getDivisors(18)); 
